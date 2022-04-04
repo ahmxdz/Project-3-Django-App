e8803b85d967669d492b81fdf11a5f2569bf1cd6
ahmxdz <ahmedali6342@gmail.com>
2022-04-04 16:09:45 -0400

    models complete

--- portfolio_tracker/portfolio_tracker_app/portfolio_tracker_app/settings.py
+++ portfolio_tracker/portfolio_tracker_app/portfolio_tracker_app/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio_tracker_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolioTracker',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5435'
     }
 }
 
