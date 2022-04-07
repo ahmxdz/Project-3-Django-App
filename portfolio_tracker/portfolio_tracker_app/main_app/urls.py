9d55ae428f1a07e20b057820a14967b900b05953
igeorge <george.haddad77@yahoo.com>
2022-04-07 14:53:33 -0400

    Details Page + Full CRUD Update
    - Details Page added
    - Confirm delete page added
    - Update and Delete Functionality added

--- portfolio_tracker/portfolio_tracker_app/main_app/urls.py
+++ portfolio_tracker/portfolio_tracker_app/main_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('portfolio/', views.index, name='index'),
-    # path('portfolio/add', views.add_to_portfolio, name='addStocks'),
     path('portfolio/add', views.AddStock.as_view(), name='addStocks'),
-    path('portfolio/update/', views.StockUpdate.as_view(), name='stocks_update'),
-    path('portfolio/delete/', views.StockDelete.as_view(), name='stocks_delete'),
+    path('portfolio/<int:stock_id>/', views.portfolio_detail, name='detail'),
+    path('portfolio/<int:pk>/update/', views.StockUpdate.as_view(), name='stock_update'),
+    path('portfolio/<int:pk>/delete/', views.StockDelete.as_view(), name='stock_delete'),
 ]
--- portfolio_tracker/portfolio_tracker_app/portfolio_tracker_app/settings.py
+++ portfolio_tracker/portfolio_tracker_app/portfolio_tracker_app/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '5435'
+        'PORT': '5432'
     }
 }
 
