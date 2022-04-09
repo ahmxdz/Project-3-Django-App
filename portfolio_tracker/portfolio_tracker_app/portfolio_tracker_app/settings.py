4f730da79676b6d7dbc8ae048558a02aaccd335d
igeorge <george.haddad77@yahoo.com>
2022-04-09 14:53:55 -0400

    User Authentication Added

--- portfolio_tracker/portfolio_tracker_app/portfolio_tracker_app/settings.py
+++ portfolio_tracker/portfolio_tracker_app/portfolio_tracker_app/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = '/portfolio/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
