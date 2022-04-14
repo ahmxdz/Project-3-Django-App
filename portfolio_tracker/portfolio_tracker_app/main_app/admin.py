e97dc5e45aa67fa1f400e4b244b202cb02e8b4de
ahmxdz <ahmedali6342@gmail.com>
2022-04-13 21:45:24 -0400

    css updated

--- portfolio_tracker/portfolio_tracker_app/main_app/admin.py
+++ portfolio_tracker/portfolio_tracker_app/main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . models import Stock, Crypto, Portfolio
 # Register your models here.
+
+admin.site.register(Stock)
+admin.site.register(Crypto)
+admin.site.register(Portfolio)
